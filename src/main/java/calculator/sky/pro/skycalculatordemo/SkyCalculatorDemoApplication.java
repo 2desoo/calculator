5378d1c38c2308a1e1f17f88a3fd9fe38267faac
desoo <marcus60.2001@gmail.com>
2024-01-21 19:46:23 +0300

    Initial commit

---
+++ src/main/java/calculator/sky/pro/skycalculatordemo/SkyCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package calculator.sky.pro.skycalculatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyCalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyCalculatorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calculator/sky/pro/skycalculatordemo/SkyCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.sky.pro.skycalculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
